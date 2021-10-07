1e8a0ebfcaff62d5c2421a6b22a00791fc7bde0c
Sorawong L <Sorawong2543@gmail.com>
2021-10-07 13:48:03 +0700

    Link component
    import Link from 'next/link'
    
    e.g. <Link href='/'><a>Home</a></Link>
    
    (in text, crop with <a></a>)

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,21 @@
+import Link from "next/link"
+
 function Home() {
-  return <h1>Home page</h1>
+  return (
+    <>
+      <Link href="/">
+        <a>Home </a>
+      </Link>
+      <Link href="/about">
+        <a>About </a>
+      </Link>
+      <Link href="/blog">
+        <a>Blog </a>
+      </Link>
+      <hr />
+      <h1>Home Page</h1>
+    </>
+  )
 }
 
 export default Home
