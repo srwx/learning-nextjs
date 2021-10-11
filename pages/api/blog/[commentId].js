52e442e5b5eddb2715b80b0fa3bc59b888e59c11
Sorawong L <Sorawong2543@gmail.com>
2021-10-11 17:56:40 +0700

    API Pre-rendering

--- pages/api/blog/[commentId].js
+++ pages/api/blog/[commentId].js
@@ -2,8 +2,14 @@ import { data } from "../../../db"
 
 function handler(req, res) {
   const { commentId } = req.query
+  console.log(`commentId : ${commentId}`)
   if (req.method === "GET") {
     const comment = data.find((i) => i.id === parseInt(commentId))
+    console.log(`in api/blog/[commentId]`)
+    console.log(comment)
+    console.log(
+      `--------------------------------------------------------------`
+    )
     res.status(200).json(comment)
   } else if (req.method === "DELETE") {
     const deletedComment = data.find((i) => i.id === parseInt(commentId))
--- pages/blog/[commentId].js
+++ pages/blog/[commentId].js
@@ -1,5 +1,40 @@
-function Comment() {
-  return <div>Enter</div>
+// =============================== This page is Pre-Rendering ===============================
+import { data } from "../../db"
+import axios from "axios"
+
+function Comment(props) {
+  const { comment } = props
+  return (
+    <div>
+      {comment.id}. {comment.text}
+    </div>
+  )
 }
 
+export const getStaticPaths = async () => {
+  return {
+    paths: [
+      { params: { commentId: "1" } },
+      { params: { commentId: "2" } },
+      { params: { commentId: "3" } },
+    ],
+    fallback: false,
+  }
+}
+
+export const getStaticProps = async (context) => {
+  const { commentId } = context.params
+
+  const comment = data.find((i) => i.id === parseInt(commentId))
+
+  // ไม่ควรใช้ axios แบบด้านล่าง เพราะในเมื่อไฟล์นี้เรามี db.js อยู่แล้ว ถ้าเราใช้ .find() หาตรงๆเลย จะเร็วกว่า เพราะไม่ต้องส่งรีเควสไปหา /api/blog
+  // const { data } = await axios.get(
+  //   `http://localhost:3000/api/blog/${commentId}`
+  // )
+  return {
+    props: {
+      comment,
+    },
+  }
+}
 export default Comment
